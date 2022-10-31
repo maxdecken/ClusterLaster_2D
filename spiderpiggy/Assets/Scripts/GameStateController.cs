eeda5b9f4382f489f8affca55c88d8a8ae46dcbe
Max Decken <max.decken40@gmail.com>
2022-10-31 10:23:59 +0100

    - basic GameStateController und Automatic LevelBuilder

---
+++ spiderpiggy/Assets/Scripts/GameStateController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateController : MonoBehaviour
+{
+    [SerializeField] private GameObject player = null;
+    [SerializeField] private GameObject sceneComponentPrefab = null;
+    [SerializeField] private int sceneElementWidth = 40;
+    private int latestPositionAdded = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Add new SceneElement if needed (only working in the right movement direction)
+        if(player.transform.position.x >= latestPositionAdded + (sceneElementWidth/4)){
+            int newPos = latestPositionAdded + sceneElementWidth;
+            Instantiate(sceneComponentPrefab, new Vector2(newPos, 0), Quaternion.identity);
+            latestPositionAdded = newPos;
+        }
+
+        //Remove unused Elements afterwards
+        foreach(GameObject sceneComponent in GameObject.FindGameObjectsWithTag("SceneComponent")){
+            if(sceneComponent.transform.position.x <= player.transform.position.x - sceneElementWidth*2){
+                Destroy(sceneComponent);
+            }
+        }
+    }
+}
--- spiderpiggy/Assets/Scripts/Player.cs
+++ spiderpiggy/Assets/Scripts/Player.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D playerRigidbody = null;
-    [SerializeField] float strenghtMove = 10;
-    [SerializeField] float strenghtJump = 100;
+    [SerializeField] private Rigidbody2D playerRigidbody = null;
+    [SerializeField] private float strenghtMove = 10;
+    [SerializeField] private float strenghtJump = 100;
+    private int jumpesInRow = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +19,20 @@ public class Player : MonoBehaviour
     {
         //Jump
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)){
-            playerRigidbody.AddForce(new Vector2(0f,  strenghtJump));
+            if(jumpesInRow <= 4){
+                playerRigidbody.AddForce(new Vector2(0f,  strenghtJump));
+                jumpesInRow++;
+            }
         }
         //Move Left
         if(Input.GetKey(KeyCode.A)){
             playerRigidbody.AddForce(new Vector2(-strenghtMove, 0f));
+            jumpesInRow = 0;
         }
         //Move Right
         if(Input.GetKey(KeyCode.D)){
             playerRigidbody.AddForce(new Vector2(strenghtMove, 0f));
+            jumpesInRow = 0;
         }
     }
 }
