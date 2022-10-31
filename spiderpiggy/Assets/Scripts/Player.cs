3b29bf56aa6d60acca6e2baa887e7e96cb9c0e08
Max Decken <max.decken40@gmail.com>
2022-10-31 15:04:50 +0100

    - fix of some merge issues

---
+++ spiderpiggy/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D playerRigidbody = null;
+    [SerializeField] private float strenghtMove = 10;
+    [SerializeField] private float strenghtJump = 100;
+    private int jumpesInRow = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Jump
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)){
+            if(jumpesInRow <= 4){
+                playerRigidbody.AddForce(new Vector2(0f,  strenghtJump));
+                jumpesInRow++;
+            }
+        }
+        //Move Left
+        if(Input.GetKey(KeyCode.A)){
+            playerRigidbody.AddForce(new Vector2(-strenghtMove, 0f));
+            jumpesInRow = 0;
+        }
+        //Move Right
+        if(Input.GetKey(KeyCode.D)){
+            playerRigidbody.AddForce(new Vector2(strenghtMove, 0f));
+            jumpesInRow = 0;
+        }
+    }
+}
