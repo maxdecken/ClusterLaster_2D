a2d2f118c2169e40963e3bd2d1bde21f536efa02
Max Decken <max.decken40@gmail.com>
2022-11-11 21:16:22 +0100

    - added new scene prefans and random placemnet, adjusted the game state controller accordingly, also indroduced music to the scene

--- spiderpiggy/Assets/Scripts/GameStateController.cs
+++ spiderpiggy/Assets/Scripts/GameStateController.cs
@@ -6,9 +6,13 @@ using UnityEngine.SceneManagement;
 public class GameStateController : MonoBehaviour
 {
     [SerializeField] private GameObject player = null;
-    [SerializeField] private GameObject sceneComponentPrefab = null;
+    [SerializeField] private GameObject[] sceneComponentPrefab = null;
+    [SerializeField] private GameObject sceneComponentBackgroundPrefab = null;
     [SerializeField] private int sceneElementWidth = 40;
+    [SerializeField] private int sceneBackgroundElementWidth = 150;
     private int latestPositionAdded = 0;
+    private int latestBackgroundPositionAdded = 0;
+    private int lastComponentAdded = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,14 +25,41 @@ public class GameStateController : MonoBehaviour
         //Add new SceneElement if needed (only working in the right movement direction)
         if(player.transform.position.x >= latestPositionAdded){
             int newPos = latestPositionAdded + sceneElementWidth;
-            Instantiate(sceneComponentPrefab, new Vector2(newPos, 0), Quaternion.identity);
+
+            //random add new component
+            int randomComponent = -1;
+            do
+            {
+                randomComponent = Random.Range(0, sceneComponentPrefab.Length-1);
+            }
+            while(lastComponentAdded == randomComponent);
+
+            Instantiate(sceneComponentPrefab[randomComponent], new Vector2(newPos, 0), Quaternion.identity);
+
             latestPositionAdded = newPos;
+            lastComponentAdded = randomComponent;
+        }
+
+        //Add new SceneBackgroundElement if needed (only working in the right movement direction)
+        if(player.transform.position.x >= latestBackgroundPositionAdded){
+            int newPos = latestBackgroundPositionAdded + sceneBackgroundElementWidth;
+
+            Instantiate(sceneComponentBackgroundPrefab, new Vector2(newPos, 0), Quaternion.identity);
+
+            latestBackgroundPositionAdded = newPos;
         }
 
         //Remove unused Elements afterwards
         foreach(GameObject sceneComponent in GameObject.FindGameObjectsWithTag("SceneComponent")){
             if(sceneComponent.transform.position.x <= player.transform.position.x - sceneElementWidth*2){
                 Destroy(sceneComponent);
+            }
+        }
+
+        //Remove unused BackgroundElements afterwards
+        foreach(GameObject backgroundComponent in GameObject.FindGameObjectsWithTag("SceneComponentBackground")){
+            if(backgroundComponent.transform.position.x <= player.transform.position.x - sceneBackgroundElementWidth- sceneBackgroundElementWidth/2){
+                Destroy(backgroundComponent);
             }
         }
 
