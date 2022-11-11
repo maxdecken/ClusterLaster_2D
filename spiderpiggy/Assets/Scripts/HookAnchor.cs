f2b5ed8727a697a940ee562db34cff6962cc5d67
COLIN_GARMS <colin.garms@online.de>
2022-11-11 17:14:49 +0100

    added Scene Colin -> RopeMovement

---
+++ spiderpiggy/Assets/Scripts/HookAnchor.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class HookAnchor : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer targetSprite;
+    [SerializeField] Transform target;
+    private Vector2 hookTarget;
+    private float maxDistance = 20f;
+    public GameObject hook;
+
+    private GameObject currentHook;
+
+    private bool ropeIsActive = false;
+    
+    private BoxCollider2D playerHitBox;
+    private Vector3 targetPosition;
+    private int layermaskToHit;
+    
+    [SerializeField] private GameObject hitSquareBetween = null;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHitBox = GetComponent<BoxCollider2D>();
+        layermaskToHit = 1 << LayerMask.NameToLayer("Hookable");
+        Debug.Log(layermaskToHit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setTargetPosition();
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            if (ropeIsActive == false)
+            {
+                currentHook = (GameObject) Instantiate(hook, transform.position, Quaternion.identity);
+
+                currentHook.GetComponent<Rope>().hookTarget = hookTarget;
+
+                ropeIsActive = true;
+            }
+            else
+            {
+                Destroy(currentHook);
+                
+                ropeIsActive = false;
+            }
+        }
+        
+        
+       
+    }
+
+
+    private void setTargetPosition(){
+        
+        // Hook Target bestimmten
+        hookTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       
+        
+        var x = hookTarget.x;
+        var y = hookTarget.y;
+
+        //hookTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        
+        targetPosition = new Vector3(x, y, 0);
+        target.transform.position = targetPosition;
+        
+        
+        
+        
+        if (!targetSprite.enabled)
+        {
+            targetSprite.enabled = true;
+        }
+        
+        
+        // Hit ray um Hookables zu treffen
+        var ray = new Ray2D(transform.position, targetPosition);
+        
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, targetPosition - transform.position, maxDistance, layermaskToHit);
+        
+        Debug.DrawLine(transform.position,targetPosition);
+        
+        
+        if (hit.collider != null)
+        {
+            targetPosition = new Vector3(hit.point.x, hit.point.y, 0);
+            target.transform.position = targetPosition;
+
+            hitSquareBetween.transform.position = new Vector3(hit.point.x, hit.point.y, 0);
+
+            hookTarget = new Vector2(hit.point.x, hit.point.y);
+        }
+        else
+        {
+            // enter solution for non hit targetting
+        }
+        
+        
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+   
+}
+
---
+++ spiderpiggy/Assets/Scripts/Rope.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Serialization;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class Rope : MonoBehaviour
+{
+    [SerializeField] float speed = 0.6f;
+    [SerializeField] float distance = 0.8f;
+
+    [SerializeField] GameObject RopePivotPrefab;
+    [SerializeField] GameObject player;
+    private Rigidbody2D playerRigidbody = null;
+    [SerializeField] GameObject lastRopePivot;
+    private GameObject RopePivot;
+    public List<GameObject> RopePivotConections = new List<GameObject>();
+    private int ropePivotCount = 2;
+    private LineRenderer lineRenderer;
+
+    private Vector2 vectorToHook;
+    public Vector2 hookTarget;
+    
+    private bool finished = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player");
+
+        lastRopePivot = transform.gameObject;
+        
+        RopePivotConections.Add(transform.gameObject);
+
+        lineRenderer = GetComponent<LineRenderer>();
+        playerRigidbody = player.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards (transform.position, hookTarget, speed);
+  
+        if((Vector2)transform.position != hookTarget)
+        {
+            
+            if (Vector2.Distance(player.transform.position, lastRopePivot.transform.position) > distance)
+            {
+                
+                CreateRopePivot();
+            }   
+            
+        }else if (finished == false)
+        {
+            finished = true;
+            lastRopePivot.GetComponent<HingeJoint2D>().connectedBody = player.GetComponent<Rigidbody2D>();
+        }
+        RenderLine();
+        
+        
+        
+        //Get up while swinging
+        
+        if(Input.GetKeyDown(KeyCode.Q) && RopePivotConections.Any())
+        {
+            vectorToHook = hookTarget- (Vector2)player.transform.position;
+            
+            RopePivot.GetComponent<Rigidbody2D>().AddForce(new Vector2(vectorToHook.x*30,  vectorToHook.y*30));
+            //playerRigidbody.AddForce(new Vector2(vectorToHook.x*30,  vectorToHook.y*30));
+            Debug.Log("piggy: " + (Vector2)player.transform.position);
+            Debug.Log("hook: " + hookTarget);
+            Debug.Log("vektor: " + vectorToHook);
+            
+            //lastRopePivot.distance 
+           
+        }   
+        
+    }
+
+    // Render Line between Player and Target through Pivotpoints
+    void RenderLine()
+    {
+        lineRenderer.SetVertexCount(ropePivotCount);
+        int i;
+        for (i = 0; i < RopePivotConections.Count; i++)
+        {
+            lineRenderer.SetPosition(i, RopePivotConections[i].transform.position);
+            //Debug.Log(i);
+            //Debug.Log(RopePivotConections[i].transform.position);
+        }
+
+        lineRenderer.SetPosition(i, player.transform.position);
+    }
+
+    void CreateRopePivot()
+    {
+        
+        Vector2 pos2Create = player.transform.position - lastRopePivot.transform.position;
+        pos2Create.Normalize();
+        pos2Create *= distance;
+        pos2Create += (Vector2)lastRopePivot.transform.position;
+
+        RopePivot = (GameObject) Instantiate(RopePivotPrefab, pos2Create, Quaternion.identity);
+        
+        RopePivot.transform.SetParent(transform);
+        
+        lastRopePivot.GetComponent<HingeJoint2D>().connectedBody = RopePivot.GetComponent<Rigidbody2D>();
+
+        lastRopePivot = RopePivot;
+        
+        RopePivotConections.Add(lastRopePivot);
+        ropePivotCount++;
+    }
+
+
+    public void DestroyRope()
+    {
+        Destroy(RopePivot.transform.parent);
+    }
+    
+}
